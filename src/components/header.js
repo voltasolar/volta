a567ed9e6a45b6f4b57953798916a65b672e5e54
Oswaldo Zarate <2448407-hiwaldo89@users.noreply.gitlab.com>
2020-07-02 13:02:25 -0500

    change click to clic

--- src/components/header.js
+++ src/components/header.js
@@ -228,7 +228,7 @@ const Header = () => {
             </a>
           </div>
           <button onClick={() => scrollTo("#contacto")}>
-            Da click e instala tus paneles
+            Da clic e instala tus paneles
           </button>
         </div>
         <div className="bottom-bar">
