41cdce850c5a2f955221f2fdf979f0bfc700ea97
Oswaldo Zarate <2448407-hiwaldo89@users.noreply.gitlab.com>
2021-02-12 16:18:20 -0600

    fix style

--- src/components/quoteGenerator.js
+++ src/components/quoteGenerator.js
@@ -60,15 +60,7 @@ const QuoteGenerator = () => {
           flex-wrap: wrap;
           overflow: hidden;
           .sr-only {
-            position: absolute;
-            width: 1px;
-            height: 1px;
-            padding: 0;
-            margin: -1px;
-            overflow: hidden;
-            clip: rect(0, 0, 0, 0);
-            white-space: nowrap;
-            border: 0;
+            display: none;
           }
           .form-wrapper {
             background-color: #f6f6f6;
